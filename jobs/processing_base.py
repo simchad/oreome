329f78747ff5879be5fb2d6dd7c5485e3edbfc18
simga <simhc0714@gmail.com>
2023-01-27 14:10:11 +0900

    2022-01-27 r405 14:10
    - Add py file for python-idle

--- jobs/processing_base.py
+++ jobs/processing_base.py
@@ -235,7 +235,7 @@ class process_base:
 
 if __name__ == "__main__":
     # evidence, peptides, proteingroups.txt 가 있는지 확인하고 실행할 수 있어야.
-    txtpath = './raw_files/TestSample/202212_DKU/A549/'
+    txtpath = './raw_files/'
     job = process_base(txtpath)
     job.proteinGroups_base()
     #job.peptides_base()
